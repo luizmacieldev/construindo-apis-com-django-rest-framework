8b5ec3b5aa2125706496a27a4a56a43a5ac85b68
luizlgam <lgamjoeperry@gmail.com>
2019-10-31 01:30:22 -0300

    Added React Project

---
+++ react/my_app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Autores from './components/Autores';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Autores />
+    </div>
+  );
+}
+
+export default App;
---
+++ react/my_app/src/components/Autores.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import axios from 'axios';
+export default class Autores extends Component {
+    state = {
+        autores:[]
+    }
+    componentDidMount(){
+        axios.get('http://127.0.0.1:8000/artigos_app/autor/')
+        .then(res => {
+            const autores = res.data;
+            this.setState({autores})
+        })
+    }
+    render() {
+        return (
+            <div>
+                <h1 className="text-center text-info">Lista de Autores</h1>
+           
+                <ul className="list-group">
+                {this.state.autores.map(autor =>
+                <div style={{textAlign:"left"}} key={autor.id}>
+                <li className="list-group-item" > {autor.nome}</li>
+                <li className="list-group-item" >{autor.endereco}</li>
+                <li className="list-group-item" >{autor.site === null || autor.site === ''?'Não Informado':autor.site}</li>
+                <li className="list-group-item" >{autor.email}</li>
+                <br />
+                </div>
+                )}
+                </ul>
+               
+               
+            </div>
+        )
+    }
+}
